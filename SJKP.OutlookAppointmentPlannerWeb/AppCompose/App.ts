ebc860cbdeb6134be1104c9d589fd8cf9aa22579
Simon J.K. Pedersen <mail@sjkp.dk>
2014-11-21 02:34:51 +0100

    init

---
+++ SJKP.OutlookAppointmentPlannerWeb/AppCompose/App.ts
@@ -0,0 +1,26 @@
+﻿/* Common app functionality */
+
+export module app {
+    // Common initialization function (to be called from each page)
+    export var initialize = function () {
+        $('body').append(
+            '<div id="notification-message">' +
+            '<div class="padding">' +
+            '<div id="notification-message-close"></div>' +
+            '<div id="notification-message-header"></div>' +
+            '<div id="notification-message-body"></div>' +
+            '</div>' +
+            '</div>');
+
+        $('#notification-message-close').click(function () {
+            $('#notification-message').hide();
+        });
+
+    };
+    // After initialization, expose a common notification function
+    export var showNotification = function (header, text) {
+        $('#notification-message-header').text(header);
+        $('#notification-message-body').text(text);
+        $('#notification-message').slideDown('fast');
+    };
+};
---
+++ SJKP.OutlookAppointmentPlannerWeb/AppCompose/AppBootstrap.ts
@@ -0,0 +1,53 @@
+﻿/// <reference path="../Scripts/typings/requirejs/require.d.ts" />
+///<reference path='App.ts'/>
+///<reference path='Home/Home.ts'/>
+
+/**
+ * Main entry point for RequireJS
+ */
+declare var Office: any;
+
+require.config({
+
+    //baseUrl: '/Scripts/',
+
+    paths: {
+        //main libraries
+        jquery: '/Scripts/jquery-1.9.1',
+        knockout: '/Scripts/knockout-3.2.0',
+
+        ////shortcut paths
+        //templates: '../templates',
+        //data: '../data',
+
+        ////require plugins
+        //text: '../vendor/require/text',
+        //tpl: '../vendor/require/tpl',
+        //json: '../vendor/require/json',
+        //hbs: '../vendor/require-handlebars-plugin/hbs'
+    },
+
+    shim: {
+        jquery: {
+            exports: '$'
+        },
+        'knockout': {            
+            exports: 'ko'
+        }
+    }
+}); 
+
+require(
+    [
+        'Home/Home',
+        'jquery',
+        'knockout',
+    ],
+    (Home, $, ko) => {
+        'use strict';
+        (<any>window).ko = ko;
+        var home = new Home.Home();
+        Office.initialize = home.initialize;
+       
+    }
+    ); 
